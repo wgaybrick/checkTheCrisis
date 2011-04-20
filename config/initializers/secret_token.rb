065739125411596be62418b9d69d46c3292caa56
Will Gaybrick <william.gaybrick@gmail.com>
2011-04-20 11:32:44 -0400

    First commig

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheckTheCrisis::Application.config.secret_token = '72ae0af2cb250e91f55deec53fe5cd97b4b7d50e9a1fe8e627fd25ee37907261f71003faf202edcc7d9f9d145f45bafed6f42e639dcaf053cfecec98416c368a'
